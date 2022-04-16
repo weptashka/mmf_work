8c74d26b3f3d8e18eafe860e7dc92198b04da35e
pollylaptop <p.logash@gmail.com>
2022-04-17 00:21:59 +0300

    feat: add methods dir names; add vych lab1

--- java_labs/src/com/bsu/java/vychka/lab1/Main.java
+++ java_labs/src/com/bsu/java/vychka/lab1/Main.java
@@ -1,2 +1,12 @@
-package com.bsu.java.vychka.lab1;public class Main {
+package com.bsu.java.vychka.lab1;
+import com.bsu.java.vychka.lab1.Mobile;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Mobile mobile1 = new Mobile("Xiaomi", "RedmiNote8Pro");
+        Mobile.Properties prop1 = mobile1.new Properties("June 2019", 200, "Grey", 500);
+        prop1.showInfo();
+    }
+
 }
