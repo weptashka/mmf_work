89cd4a43540a621e6e3f7d42dea08b8be2861433
Polly Childrenroom <p.logash@gmail.com>
2023-01-29 04:59:32 +0300

    add a lot

---
+++ C++_1-2sem/проги с++/11.7.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+/*«адача 7. ¬водить буквы. ¬ыполнение завершить, когда будет введена латинска€ СnТ.*/
+int main()
+{
+	char a;
+
+	do{
+		cout<<"vvodite latinskie bykvy:"<<endl;
+		cin>>a;}
+	while (a!='n');
+
+	cout<<"vy vveli n!"<<endl;
+	return 0;
+}
+
